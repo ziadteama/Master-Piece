d669680429338f7eb85fd9412e176bdabe609e9a
Ziad Teama <ziadbasaria600@gmail.com>
2025-05-07 05:35:11 +0300

    title and icon

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -3,13 +3,14 @@ import Hero from "../components/Hero";
 import ServicesGrid from "../components/ServiceGrid";
 import StartProject from "../components/StartProject";
 import Footer from "../components/Footer";
+import serviceHero from "../assets/images/services-hero.png";
 
 const Services = () => {
     return (
      <>
 
      <Navbar />
-     <Hero text="Services" img="https://images.pexels.com/photos/1103970/pexels-photo-1103970.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+     <Hero img={serviceHero} text="Services"/>
      <ServicesGrid /> 
      <StartProject />
      <Footer />
